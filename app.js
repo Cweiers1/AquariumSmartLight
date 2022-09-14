649823029354016582619c829602748cec1ab044
Cweiers1 <Cweiers1@gmail.com>
2022-09-13 23:40:16 -0400

    Did lots of stuff

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-//const Gpio = require("pigpio").Gpio;
+const Gpio = require("pigpio").Gpio;
 const path = require("path");
 const fs = require("fs");
 let schedule;
@@ -15,6 +15,12 @@ const currentDay = () => {
 	let currentTime = new Date(Date.now());
 	return currentTime.getDay();
 }
+
+fs.readFile("Schedule.json", (err, data) => {
+	if (err) throw err;
+	schedule = JSON.parse(data);
+})
+
 //reads file and sends event when loaded
 fs.watchFile("Schedule.json", () => {
 	fs.readFile("Schedule.json", (err, data) => {
@@ -23,12 +29,6 @@ fs.watchFile("Schedule.json", () => {
 		loadNextEvent();
 	});
 })
-
-fs.readFile("Schedule.json", (err, data) => {
-	if (err) throw err;
-	schedule = JSON.parse(data);
-	loadNextEvent();
-});
 
 function loadNextEvent() {
 	for (let i=0; i < Object.keys(schedule).length-1; i++) {
@@ -61,8 +61,7 @@ app.use(
 
 app.use(
 	"/js",
-	express.static(path.join(__dirname, "node_modules/bootstrap/dist/js")),
-	express.static(path.join(__dirname, "node_modules/@jaames/iro/dist"))
+	express.static(path.join(__dirname, "node_modules/bootstrap/dist/js"))
 )
 
 app.use(express.json())
@@ -76,10 +75,35 @@ app.get("/schedule", (req, res) => {
 })
 
 app.post("/schedule", (req, res) => {
-	
+	console.log(req.json)
+	// schedule.push(req.json)
 })
 
+app.delete("/schedule", async (req, res) => {
+	const event = req.body.alarm;
+	schedule.events.splice(event, 1);
+	console.log(schedule)
+	await arrObjSort(schedule.events, "time")
+	writeJson(schedule);
+	res.json(schedule);
+})
 
-
-
+async function writeJson(object)  {
+	const data = JSON.stringify(object);
+	fs.writeFile("Schedule.json", data, (err) => {
+		if (err) throw err;
+		return;
+	})
+}
+function arrObjSort(arr, key) {
+	arr.sort((a, b) => {
+		if (a[key] < b[key]) {
+			return -1
+		}
+		if (a[key] > b[key]) {
+			return 1;
+		}
+		return 0;
+	})
+}
 
--- public/script.js
+++ public/script.js
@@ -6,8 +6,33 @@ const color = document.getElementById("color");
 const kelvinSelect = document.getElementById("kelvinSelect");
 const rgbToggle = document.getElementById("rgbToggle");
 const wwToggle = document.getElementById("wwToggle");
+const eventNum = document.getElementById("eventNum");
+const weeklyRadio = document.getElementById("weekly");
+const dailyRadio = document.getElementById("daily");
+const weeklySelect = document.getElementById("weeklySelect");
+const time = document.getElementById("timeSelect");
+const deleteNum = document.getElementById("deleteNum");
 
+//TO DO: make prompt when deleting asking if sure
+// const modal = new bootstrap.Modal(document.getElementById("deleteModal"), {})
 
+//used when creating new alarms
+class alarm {
+  constructor(status, time, location, rgbColor, kelvin) {
+    this.status = status;
+    this.time = time;
+    this.location = location;
+    if (rgbColor) {
+      this.rgbColor = rgbColor;
+    } 
+    if (kelvin) {
+      this.kelvin = kelvin;
+    }
+  }
+}
+
+//defines our color selectors
+//color wheel + saturation slider
 const iroColorPicker =  new iro.ColorPicker(colorWheel, {
   layout: [
     {
@@ -20,7 +45,7 @@ const iroColorPicker =  new iro.ColorPicker(colorWheel, {
     }
   ]
 })
-
+//seperate instance of iro, only kelvin slider
 const iroKelvinPicker = new iro.ColorPicker("#kelvinSelect", {
   layout: [
     {
@@ -32,44 +57,7 @@ const iroKelvinPicker = new iro.ColorPicker("#kelvinSelect", {
   ]
 })
 
-
-eventSelect.addEventListener("change", () => {
-  if ((eventSelect.value == "light") && (eventType.value == "change")) {
-    color.setAttribute("class", "d-block")
-  } else {  
-    color.setAttribute("class", "d-none")
-  }
-})
-
-eventType.addEventListener("change", () => {
-  if ((eventSelect.value == "light") && (eventType.value == "change")) {
-    color.setAttribute("class", "d-block")
-  } else {
-    color.setAttribute("class", "d-none")
-  }
-})
-
-rgbToggle.addEventListener("change", () => {
-  if (rgbToggle.checked) {
-    colorPicker.setAttribute("class", "d-block")
-  } else {
-    colorPicker.setAttribute("class", "d-none")
-  }
-})
-
-wwToggle.addEventListener("change", () => {
-  if (wwToggle.checked) {
-    kelvinSelect.setAttribute("class", "d-block my-2")
-  } else {
-    kelvinSelect.setAttribute("class", "d-none")
-  }
-})
-
-const alarmList = document.getElementById("alarmList");
-const listItem = document.getElementById("listItem");
-const fragment = new DocumentFragment();
-let schedule;
-
+//fetches Schedule.json from server, passes renderer
 fetch(`http://${location.hostname}:5000/schedule`)
 .then((response) => response.json())
 .then((data) => {
@@ -77,11 +65,20 @@ fetch(`http://${location.hostname}:5000/schedule`)
  renderAlarms(data);
 })
 
+const alarmList = document.getElementById("alarmList");
+const listItem = document.getElementById("listItem");
+const fragment = new DocumentFragment();
+let schedule;
+
+//takes 24 hour time from server and converts to am/pm for user readiablity
 function renderAlarms(data) {
+  // TO DO: remove unneeded 0 on times sooner then 10am
   for(let i=0; i < data.events.length; i++) {
     let subFragment = listItem.content.cloneNode(true);
+    let btnarr = subFragment.querySelectorAll("button");
     let alarmHour = schedule.events[i].time.substring(0,2);
-    let alarmMin = schedule.events[i].time.substring(3,5);
+    let alarmMin = schedule.events[i].time.substring(2,4);
+
     if (alarmHour > 12) {
       subFragment.querySelector("div h2").innerHTML = `${alarmHour-12}:${alarmMin}`;
       subFragment.querySelector("div p").innerHTML = "pm";
@@ -92,9 +89,167 @@ function renderAlarms(data) {
         subFragment.querySelector("div h2").innerHTML = `${alarmHour}:${alarmMin}`;
         subFragment.querySelector("div p").innerHTML = "am";
       }
+
+    btnarr[0].setAttribute("onclick", `modalDelete(${i})`);
+    btnarr[1].setAttribute("onclick", `modalStart(${i})`);
     fragment.appendChild(subFragment);
   }
   alarmList.appendChild(fragment);
+} 
+
+// all this code handles showing and hiding elements based on form selection
+eventSelect.addEventListener("change", () => {
+  if ((eventSelect.value == "light") && (eventType.value == "on")) {
+    color.setAttribute("class", "d-block")
+  } else {  
+    color.setAttribute("class", "d-none")
+  }
+})
+eventSelect.addEventListener("change", update);
+eventType.addEventListener("change", update);
+rgbToggle.addEventListener("change", update);
+wwToggle.addEventListener("change", update);
+dailyRadio.addEventListener("change", updateWeek);
+weeklyRadio.addEventListener("change", updateWeek);
+
+function update() {
+  updateSlider();
+  updateSwitch();
+  updateWheel();
 }
 
-document.querySelector
+function updateToggle() {
+
+}
+
+function updateSwitch() {
+  if ((eventSelect.value == "light") && (eventType.value == "on")) {
+    color.setAttribute("class", "d-block")
+  } else {
+    color.setAttribute("class", "d-none")
+  }
+}
+
+function updateWheel() {
+  if (rgbToggle.checked) {
+    colorPicker.setAttribute("class", "d-block")
+  } else {
+    colorPicker.setAttribute("class", "d-none")
+  }
+}
+
+function updateSlider() {
+  if (wwToggle.checked) {
+    kelvinSelect.setAttribute("class", "d-block my-2")
+  } else {
+    kelvinSelect.setAttribute("class", "d-none")
+  }
+}
+
+function updateWeek() {
+  if (weeklyRadio.checked) {
+    weeklySelect.setAttribute("class", "mt-3")
+  } else if (dailyRadio.checked) {
+    weeklySelect.setAttribute("class", "d-none")
+  }
+}
+
+function modalDelete(event) {
+//pass event # to deleteEvent() when user confirms via modal
+deleteNum.value = event;
+}
+
+async function deleteEvent() {
+// event = eventNum to delete
+let bodymsg = {
+  alarm : deleteNum.value
+}
+
+  fetch(`http://${location.hostname}:${location.port}/schedule`, {
+    method:"DELETE",
+    cache:"no-cache",
+    headers: {
+      "Content-Type":"application/json"
+    },
+    body: JSON.stringify(bodymsg)
+  }).then((response) => response.json())
+    .then((data) => {
+    schedule = data;
+    clearAlarm();
+    renderAlarms(data);
+  })
+}
+
+function modalStart(event) {
+  let currentEvent = schedule.events[event];
+  eventNum.value = event;
+  eventType.value = currentEvent.status;
+  eventSelect.value = currentEvent.location;
+
+  if ("rgbColor" in currentEvent && currentEvent.rgbColor != false) {
+    console.log(currentEvent.rgbColor)
+    rgbToggle.checked = true;
+    iroColorPicker.color.hexString = currentEvent.rgbColor;
+  }
+  
+  if ("kelvin" in currentEvent && currentEvent.kelvin != false) {
+    wwToggle.checked = true;
+    iroKelvinPicker.color.kelvin = currentEvent.kelvin;
+  }
+  updateSlider();
+  updateSwitch();
+  updateWheel();
+  updateWeek();
+}
+
+//reverts modal to default state when hidden
+const modal = document.getElementById("alarmModal")
+
+
+  modal.addEventListener("hide.bs.modal", () => {
+    document.getElementById("modalTitle").innerHTML = "Edit Event"
+    eventNum.value = "";
+    eventType.value = "selected";
+    eventSelect.value = "selected";
+    rgbToggle.checked = false;
+    wwToggle.checked = false;
+  })  
+
+async function createAlarm() {
+  
+  console.log("eeee")
+ const newAlarm = new alarm(
+  eventType, 
+  time, 
+  eventSelect, 
+  iroColorPicker.color.hexString, 
+  iroKelvinPicker.color.kelvin
+  );
+
+  fetch(`http://${location.hostname}:${location.port}/schedule`, {
+    method:"POST",
+    cache:"no-cache",
+    headers: {
+      "Content-Type":"application/json"
+      },
+    body: JSON.stringify(newAlarm)
+  })
+  .then((response) => response.json())
+  .then((data) => {
+    schedule = data;
+    clearAlarm();
+    renderAlarms(schedule.events)
+  })
+}
+
+function clearAlarm() {
+  while (alarmList.firstChild) {
+    alarmList.lastChild.remove()
+  }
+}
+
+const submit = document.getElementById("submit");
+function newAlarmModal() {
+  submit.setAttribute("onclick", "createAlarm");
+  document.getElementById("modalTitle").innerHTML = "Add Event"
+}
